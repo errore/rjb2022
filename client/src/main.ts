458f2d70fd2c107d22b520dd104ce291aac64719
Errore <error_wings@qq.com>
2022-06-26 15:48:18 +0800

    Rectangle update

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,47 +2,66 @@ import { load } from "@amap/amap-jsapi-loader";
 import "@amap/amap-jsapi-types";
 
 
-let tiles_handler = function (this: GlobalEventHandlers, ev: MouseEvent) {
-    console.log(this);
-}
-
 load({ //首次调用 load
     key: '1c96d5a705a60c162735d89fcb7a644d',//首次load key为必填
     version: '2.0',
-    plugins: ['AMap.Scale', 'AMap.ToolBar']
+    plugins: ['AMap.Scale', 'AMap.ToolBar', 'AMap.DistrictLayer', 'AMap.RectangleEditor']
 }).then((AMap) => {
     let map: AMap.Map = new AMap.Map('container');
-    let tiles: AMap.TileLayer = new AMap.TileLayer.Flexible({
-        cacheSize: 300,
-        zIndex: 200,
-        createTile: function (x: number, y: number, z: number, success: (arg0: HTMLCanvasElement) => void, fail: any) {
-            let c = document.createElement('canvas');
-            c.width = c.height = 256;
 
-            let cxt = c.getContext("2d") as CanvasRenderingContext2D;
-            cxt.font = "15px Verdana";
-            cxt.fillStyle = "#ff0000";
-            cxt.strokeStyle = "#FF0000";
-            cxt.strokeRect(0, 0, 256, 256);
-            cxt.fillText('(' + [x, y, z].join(',') + ')', 10, 30);
-            c.onclick = tiles_handler
-
-            // 通知API切片创建完成
-            success(c);
+    let district = new AMap.DistrictLayer.Province(
+        {
+            zIndex: 10,
+            SOC: 'CHN',
+            depth: 1,
+            styles: {
+                'nation-stroke': '#22ffff',
+                'coastline-stroke': [0.8, 0.63, 0.94, 1],
+                'province-stroke': 'red',
+                'fill': ''
+            }
         }
-    });
+    );
 
     map.setLayers(
         [
             new AMap.TileLayer.Satellite(),
-            tiles
+            district
         ]);
 
     map.addControl(new AMap.Scale());
     map.addControl(new AMap.ToolBar());
-    map.add(new AMap.Marker({
-        position: map.getCenter()
-    }));
+
+    var southWest = new AMap.LngLat(116.356449, 39.859008)
+    var northEast = new AMap.LngLat(116.417901, 39.893797)
+
+    var bounds = new AMap.Bounds(southWest, northEast)
+
+    var rectangle: AMap.Rectangle = new AMap.Rectangle({
+        bounds: bounds,
+        strokeColor: 'red',
+        strokeWeight: 6,
+        strokeOpacity: 0.5,
+        strokeStyle: 'solid',
+        fillColor: '',
+        cursor: 'pointer',
+        zIndex: 50,
+    })
+
+    map.add(rectangle);
+    // 缩放地图到合适的视野级别
+    map.setFitView([rectangle])
+
+    var rectangleEditor = new AMap.RectangleEditor(map, rectangle)
+
+    document.getElementById("start_edit")?.addEventListener("click", () => rectangleEditor.open())
+    document.getElementById("end_edit")?.addEventListener("click", () => rectangleEditor.close())
+
+    rectangleEditor.on("end", ()=>{
+        // 这里重新设置Rectangle为正方形
+    })
+
 }).catch((e) => {
     console.error(e);
 });
+
