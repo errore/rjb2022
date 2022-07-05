ae9c5e650379f0f7b4a98ab1ae90695a2c04cd95
errore <error_wings@qq.com>
2022-07-05 21:22:48 +0800

    debug on server

--- server/web/settings.py
+++ server/web/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build')
+    os.path.join(BASE_DIR, '../client/build')
 ]
 
 # Default primary key field type
